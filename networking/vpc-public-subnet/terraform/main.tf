7d7c92949b07d1eba71d562054edb0d6f6827ff8
Tejas Mane <tejasmane485@gmail.com>
2025-01-11 14:33:12 +0530

    route table and sg added

--- networking/vpc-public-subnet/terraform/main.tf
+++ networking/vpc-public-subnet/terraform/main.tf
@@ -32,3 +32,53 @@ resource "aws_subnet" "public" {
     Environment = var.environment
   }
 }
+
+# Route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name        = "aws-test-vpc-public-rt"
+    Environment = var.environment
+  }
+}
+
+# Route table association
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Security Group
+resource "aws_security_group" "ec2" {
+  name        = "ec2-security-group"
+  description = "Security group for EC2 instance"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH for my IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${chmop(data.http.myip.response_body)}/32"]
+  }
+
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "ec2-security-group"
+    Environment = var.environment
+  }
+}
+
+
